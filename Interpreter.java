62ee87ffe7721ce623318dc4933364c2e64175a3
martintc <33560895+martintc@users.noreply.github.com>
2019-12-23 10:22:35 -0800

    Update and rename Parser.java to Interpreter.java

---
+++ Interpreter.java
@@ -0,0 +1,19 @@
+import java.util.Stack;
+
+public class Interpreter {
+  
+    private Stack<Operator> operators;
+    private Stack<Operands> operands;
+  
+    public Interpreter () {
+           operators = new Stack<>();
+           operands = new Stack<>();
+    }
+  
+  public void parse () {
+    
+    
+    
+  }
+  
+}
